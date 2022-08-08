8f41df65362ae864b76b60c09c1f264949a08594
Tom Roar Furunes <tom.roar@halloverden.no>
2022-08-08 16:13:43 +0200

    ADD: base bundle setup

---
+++ src/HalloVerdenSymfonyAuthenticatorRestrictorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HalloVerden\SymfonyAuthenticatorRestrictorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HalloVerdenSymfonyAuthenticatorRestrictorBundle extends Bundle {
+
+}
